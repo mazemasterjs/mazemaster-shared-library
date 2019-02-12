07456410f065e5d7e0d7c111c023ab6cae688041
James Dean <kreebog@gmail.com>
2019-02-12 15:52:44 -0500

    trying another publishing option

--- package.ps1
+++ package.ps1
@@ -1,7 +1,12 @@
-Remove-Item -Force -R ./dist
 Remove-Item -Force -R ./bin
 tsc
 mkdir ./dist
-Copy-Item ./bin/src/*.js ./dist/
-Copy-Item ./bin/src/*.js.map ./dist/
-Copy-Item ./src/*.ts ./dist/
+Copy-Item ./bin/src/*.js .
+Copy-Item ./bin/src/*.js.map .
+Copy-Item ./src/*.ts .
+
+npm publish 
+
+Remove-Item ./*.js
+Remove-Item ./*.js.map
+Remove-Item ./*.ts
