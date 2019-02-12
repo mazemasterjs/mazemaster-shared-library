f95520270623f94216928f94f05e50d622693736
James Dean <kreebog@gmail.com>
2019-02-12 16:15:47 -0500

    one more try at packaging...

---
+++ publish.bat
@@ -0,0 +1,32 @@
+@ECHO OFF
+if "%1"=="pre" (
+    echo "Pre-publish"
+
+    if EXIST "bin" (
+        echo "Clearing bin\ directory..."
+        rmdir /s /q bin
+    )
+
+    echo "Deleting .js and .js.map files from root."
+    del /q *.js
+    del /q *.js.map
+
+    echo "compiling library..."
+    tsc
+
+    echo "Copying bin\*.js and bin\*.js.map to root..."
+    copy bin\src\*.js .
+    copy bin\src\*.js.map .
+
+    echo "Pre-publish complete."
+)
+
+if "%1"=="post" (
+    echo "Post-publish"
+
+    echo "Deleting .js and .js.map files from root."
+    del /q *.js
+    del /q *.js.map
+
+    echo "Post-publish complete."
+)
