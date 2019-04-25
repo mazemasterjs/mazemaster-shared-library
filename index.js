7801526370434635569a07a27a9f843f6f3ca88a
James Dean <kreebog@gmail.com>
2019-04-25 19:21:32 -0400

    reworked publishing - added type declarations

---
+++ index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+module.exports = require('./dist/Argument');
+module.exports = require('./dist/Cell');
+module.exports = require('./dist/Config');
+module.exports = require('./dist/Endpoint');
+module.exports = require('./dist/Enums');
+module.exports = require('./dist/Helpers');
+module.exports = require('./dist/Location');
+module.exports = require('./dist/Maze');
+module.exports = require('./dist/MongoDBHandler');
+module.exports = require('./dist/Service');
