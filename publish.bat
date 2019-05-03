552068d683bfb6849337ca2ea85c3cb80b0fcb64
James Dean <kreebog@gmail.com>
2019-05-03 10:48:07 -0400

    version bumps, types included in pub script

--- publish.bat
+++ publish.bat
@@ -15,9 +15,12 @@ echo "Compiling library"
 tsc|rem
 
 echo "Copying files in bin\ to dist\"
-copy bin\*.* dist\
+xcopy /S /Y bin dist
 
 echo "Copying package files to dist\"
 copy LICENSE dist\
 copy package.json dist\
 copy README.md dist\
+copy .npmignore dist\
+
+npm publish dist|rem
