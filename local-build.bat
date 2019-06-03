9bcdb5d2a38c5a6ae3d0103701b991d7e862c992
James Dean <kreebog@gmail.com>
2019-06-03 13:06:03 -0400

    adding my local build script

---
+++ local-build.bat
@@ -0,0 +1,29 @@
+@ECHO OFF
+echo "You must stop target project if running - including any watchers!"
+echo "You will need to restart target projects for changes to take effect."
+if EXIST "dist" (
+    echo "Clearing dist\ directory"
+    rmdir /s /q dist
+)
+mkdir dist
+
+if EXIST "bin" (
+    echo "Clearing bin\ directory"
+    REM rmdir /s /q bin
+)
+
+echo "Compiling library"
+tsc|rem
+
+echo "Copying files in bin\ to dist\"
+copy bin\*.* dist\
+copy bin\types\*.* dist\
+
+echo "Copying package files to dist\"
+copy LICENSE dist\
+copy package.json dist\
+copy README.md dist\
+copy .npmignore dist\
+
+echo "Copying \dist to game-server modules folder"
+robocopy /MIR dist\ ..\game-server\node_modules\@mazemasterjs\shared-library
